3a259ddcae0b84eabf0ab47c74d8e29b3c47ce05
slemi23 <slemi@unomaha.edu>
2023-04-21 15:28:01 -0500

    Revised functions/added docstrings

--- account.py
+++ account.py
@@ -1,25 +1,52 @@
 class Account:
+    """
+    A class to represent bank account deposits and withdrawals
+    """
 
     def __init__(self, account_name: str, account_balance=0) -> None:
+        """
+        :param account_name: Name of account owner
+        :param account_balance: Starting amount of account
+        """
         self.__account_name = account_name
         self.__account_balance = account_balance
 
-    def deposit(self, amount) -> bool:
-        if self.__account_balance > 0:
+    def deposit(self, amount: float) -> bool:
+        """
+        Method that increments the account balance by the specified amount
+        :param amount: Amount to be added to account balance
+        :return: Successful/unsuccessful deposit transaction
+        """
+        if amount > 0:
             self.__account_balance += amount
             return True
         else:
             return False
 
-    def withdraw(self, amount) -> bool:
-        if self.__account_balance >= amount:
+    def withdraw(self, amount: float) -> bool:
+        """
+        Method that decrements account balance by specified amount
+        :param amount: Amount to be withdrawn from account balance
+        :return: Successful/unsuccessful withdrawal transaction
+        """
+        if amount <= 0:
+            return False
+        elif amount > self.__account_balance:
+            return False
+        else:
             self.__account_balance -= amount
             return True
-        else:
-            return False
 
-    def get_balance(self):
+    def get_balance(self) -> float:
+        """
+        Method to access account balance
+        :return: Amount in account balance
+        """
         return self.__account_balance
 
     def get_name(self) -> str:
+        """
+        Method to access name of account owner
+        :return: Name of account owner
+        """
         return self.__account_name
